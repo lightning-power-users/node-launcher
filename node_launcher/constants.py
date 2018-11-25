8d4b24ae8ae8baa1a06263f004442202b8c09cbc
Pierre Rochard <pierre@rochard.org>
2018-11-25 13:39:23 -0500

    fix mac related test failures

--- node_launcher/constants.py
+++ node_launcher/constants.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 BITCOIN_QT_PATH: Dict[str, str] = {
     'darwin': '/Applications/Bitcoin-Qt.app/Contents/MacOS/Bitcoin-Qt',
-    'windows': os.path.join(os.environ.get('Programw6432'), 'Bitcoin', 'bitcoin-qt.exe')
+    'windows': os.path.join(os.environ.get('Programw6432', ''), 'Bitcoin', 'bitcoin-qt.exe')
 }
 
 DATA_PATH: Dict[str, str] = {
