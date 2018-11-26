f9163e586c79a31353be348ee638bd80e4afb560
Pierre Rochard <pierre@rochard.org>
2018-11-25 19:00:49 -0500

    refactoring operating system constants

--- node_launcher/constants.py
+++ node_launcher/constants.py
@@ -2,6 +2,8 @@ import os
 import platform
 from typing import Dict
 
+TARGET_RELEASE = 'v0.5.1-beta-rc1'
+
 
 class OperatingSystem(object):
     def __init__(self, name: str):
@@ -23,21 +25,34 @@ class OperatingSystem(object):
 DARWIN: OperatingSystem = OperatingSystem('darwin')
 LINUX: OperatingSystem = OperatingSystem('linux')
 WINDOWS: OperatingSystem = OperatingSystem('windows')
+OPERATING_SYSTEM = OperatingSystem(platform.system())
+
+# Only relevant for Windows
+LOCALAPPDATA = os.path.abspath(os.environ.get('LOCALAPPDATA', ''))
+APPDATA = os.path.abspath(os.environ.get('APPDATA', ''))
+PROGRAMS = os.environ.get('Programw6432', '')
 
 BITCOIN_QT_PATH: Dict[OperatingSystem, str] = {
     DARWIN: '/Applications/Bitcoin-Qt.app/Contents/MacOS/Bitcoin-Qt',
-    WINDOWS: os.path.join(os.environ.get('Programw6432', ''), 'Bitcoin',
-                          'bitcoin-qt.exe')
+    LINUX: 'bitcoind',
+    WINDOWS: os.path.join(PROGRAMS, 'Bitcoin', 'bitcoin-qt.exe')
 }
 
-DATA_PATH: Dict[OperatingSystem, str] = {
+NODE_LAUNCHER_DATA_PATH: Dict[OperatingSystem, str] = {
     DARWIN: os.path.expanduser(
         '~/Library/Application Support/Node Launcher/'),
-    WINDOWS: os.path.join(os.path.abspath(os.environ.get('LOCALAPPDATA', '')),
-                          'Node Launcher'),
-    LINUX: os.path.expanduser('~/.node_launcher')
+    LINUX: os.path.expanduser('~/.node_launcher'),
+    WINDOWS: os.path.join(LOCALAPPDATA, 'Node Launcher')
 }
 
-OPERATING_SYSTEM = OperatingSystem(platform.system())
+LND_DATA_PATH: Dict[OperatingSystem, str] = {
+    DARWIN: '~/Library/Application Support/Lnd/',
+    LINUX: '~/.lnd/',
+    WINDOWS: os.path.join(LOCALAPPDATA, 'lnd')
+}
 
-TARGET_RELEASE = 'v0.5.1-beta-rc1'
+BITCOIN_DATA_PATH: Dict[OperatingSystem, str] = {
+    DARWIN: os.path.expanduser('~/Library/Application Support/Bitcoin/'),
+    LINUX: os.path.expanduser('~/.bitcoin'),
+    WINDOWS: os.path.join(APPDATA, 'Bitcoin')
+}
