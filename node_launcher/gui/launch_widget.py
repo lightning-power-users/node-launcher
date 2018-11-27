020c08328e17b95706e554ff033e4f4f5e2872e5
Pierre Rochard <pierre@rochard.org>
2018-11-27 17:56:30 -0500

    add an advertisement for lighting power users

--- node_launcher/gui/launch_widget.py
+++ node_launcher/gui/launch_widget.py
@@ -1,4 +1,6 @@
 from PySide2 import QtWidgets
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QLabel
 
 from node_launcher.gui.network_group_box import NetworkGroupBox
 
@@ -10,6 +12,14 @@ class LaunchWidget(QtWidgets.QWidget):
         self.grid = QtWidgets.QGridLayout()
         self.testnet_group_box = NetworkGroupBox('testnet', self.node_launcher)
         self.mainnet_group_box = NetworkGroupBox('mainnet', self.node_launcher)
-        self.grid.addWidget(self.testnet_group_box, 1, 1)
-        self.grid.addWidget(self.mainnet_group_box, 1, 2)
+        lpu_advertisement = 'Want a real mainnet Bitcoin faucet? Join the ' \
+                            '<a href="https://www.lightningpowerusers.com/">Lighting Power Users</a>'
+        advertisement_label = QLabel(self)
+        advertisement_label.setOpenExternalLinks(True)
+        advertisement_label.setFixedHeight(advertisement_label.height() * 2)
+        advertisement_label.setText(lpu_advertisement)
+        advertisement_label.setAlignment(Qt.AlignCenter | Qt.AlignCenter)
+        self.grid.addWidget(advertisement_label, 1, 1, 1, 2)
+        self.grid.addWidget(self.testnet_group_box, 2, 1)
+        self.grid.addWidget(self.mainnet_group_box, 2, 2)
         self.setLayout(self.grid)
