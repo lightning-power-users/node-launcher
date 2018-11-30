ea0d34f81e7a5db5beb3178ca54d771ee83fa303
Pierre Rochard <pierre@rochard.org>
2018-11-30 10:43:59 -0500

    fix get latest version bug

--- node_launcher/node_software/launcher_software.py
+++ node_launcher/node_software/launcher_software.py
@@ -1,3 +1,4 @@
+from node_launcher.constants import NODE_LAUNCHER_RELEASE
 from node_launcher.node_software.node_software import NodeSoftware
 
 
@@ -6,3 +7,5 @@ class LauncherSoftware(NodeSoftware):
         super().__init__()
         self.github_team = 'pierrerochard'
         self.github_repo = 'node-launcher'
+        if self.get_latest_release_version() is None:
+            self.get_latest_release_version = lambda: NODE_LAUNCHER_RELEASE
