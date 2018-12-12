d7f162c37128d17a5a6d4de5af909d65bb91d3f9
Pierre Rochard <pierre@rochard.org>
2018-12-12 17:34:48 -0500

    handle permission error

--- node_launcher/utilities.py
+++ node_launcher/utilities.py
@@ -7,11 +7,14 @@ from string import ascii_lowercase, digits
 
 def get_dir_size(start_path: str) -> int:
     total_size = 0
-    for entry in os.scandir(start_path):
-        if entry.is_dir(follow_symlinks=False):
-            total_size += get_dir_size(entry.path)
-        elif entry.is_file(follow_symlinks=False):
-            total_size += entry.stat().st_size
+    try:
+        for entry in os.scandir(start_path):
+            if entry.is_dir(follow_symlinks=False):
+                total_size += get_dir_size(entry.path)
+            elif entry.is_file(follow_symlinks=False):
+                total_size += entry.stat().st_size
+    except PermissionError:
+        pass
     return total_size
 
 
