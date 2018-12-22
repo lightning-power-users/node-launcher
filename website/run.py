9ead3450e4aa52cc6230a971891002cb96510357
Pierre Rochard <pierre@rochard.org>
2018-12-21 20:00:59 -0500

    add pages for website

--- website/run.py
+++ website/run.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
+
+
+@app.route('/channels')
+def channels():
+    channel_data = []
+    return render_template('channels.html', channels=channel_data)
+
+
+@app.route('/help')
+def help_route():
+    return render_template('help.html')
 
 
 freezer = Freezer(app)
-
 
 if __name__ == '__main__':
     app.debug = True
