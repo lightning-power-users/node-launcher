89c0af8810b0513abc9abf427cc267e4086b0ace
Pierre Rochard <pierre@rochard.org>
2018-12-21 20:45:20 -0500

    finish setting up index

--- website/run.py
+++ website/run.py
@@ -1,17 +1,28 @@
 from flask import render_template, Flask
 from flask_frozen import Freezer
+from google.protobuf.json_format import MessageToDict
+
+from node_launcher.node_set import NodeSet
 
 app = Flask(__name__)
+
+node_set = NodeSet('mainnet')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # noinspection PyBroadException
+    try:
+        info = MessageToDict(node_set.lnd_client.get_info())
+    except Exception as e:
+        info = None
+    return render_template('index.html', info=info)
 
 
 @app.route('/channels')
 def channels():
     channel_data = []
+
     return render_template('channels.html', channels=channel_data)
 
 
