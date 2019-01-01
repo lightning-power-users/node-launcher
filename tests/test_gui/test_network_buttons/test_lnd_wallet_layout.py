e3d571b138f3a43936a31c4b96e8a80b14a5ba6b
Pierre Rochard <pierre@rochard.org>
2019-01-01 12:50:03 -0500

    add tests for the lnd wallet layout

--- tests/test_gui/test_network_buttons/test_lnd_wallet_layout.py
+++ tests/test_gui/test_network_buttons/test_lnd_wallet_layout.py
@@ -12,7 +12,6 @@ from node_launcher.gui.network_buttons.lnd_wallet_layout import LndWalletLayout
 def lnd_wallet_layout() -> LndWalletLayout:
     node_set = MagicMock()
     node_set.network = 'mainnet'
-    node_set.lnd.macaroon_path = 'test macaroon'
     lnd_wallet_layout = LndWalletLayout(QWidget(), node_set)
     return lnd_wallet_layout
 
@@ -32,16 +31,29 @@ class TestLndWalletLayout(object):
 
     @patch('node_launcher.gui.network_buttons.lnd_wallet_layout.keyring')
     @patch('node_launcher.gui.network_buttons.lnd_wallet_layout.QInputDialog')
+    @patch('node_launcher.gui.network_buttons.lnd_wallet_layout.SeedDialog')
     def test_create_wallet(self,
+                           seed_dialog_patch: MagicMock,
                            input_dialog_patch: MagicMock,
                            keyring_patch: MagicMock,
-                           lnd_wallet_layout: LndWalletLayout, qtbot: QTest):
+                           lnd_wallet_layout: LndWalletLayout,
+                           qtbot: QTest):
         input_dialog_patch.getText = MagicMock(return_value=('password', True))
-        qtbot.mouseClick(lnd_wallet_layout.unlock_wallet_button, Qt.LeftButton)
+        qtbot.mouseClick(lnd_wallet_layout.create_wallet_button, Qt.LeftButton)
         input_dialog_patch.getText.assert_called()
         lnd_wallet_layout.node_set.lnd_client.generate_seed.assert_called()
+        seed_dialog_patch.assert_called()
         keyring_patch.set_password.assert_called()
 
-    def test_recover_wallet(self, lnd_wallet_layout: LndWalletLayout,
+    @patch('node_launcher.gui.network_buttons.lnd_wallet_layout.keyring')
+    @patch('node_launcher.gui.network_buttons.lnd_wallet_layout.QInputDialog')
+    def test_recover_wallet(self,
+                            input_dialog_patch: MagicMock,
+                            keyring_patch: MagicMock,
+                            lnd_wallet_layout: LndWalletLayout,
                             qtbot: QTest):
-        pass
+        input_dialog_patch.getText = MagicMock(return_value=('password', True))
+        qtbot.mouseClick(lnd_wallet_layout.recover_wallet_button, Qt.LeftButton)
+        input_dialog_patch.getText.assert_called()
+        lnd_wallet_layout.node_set.lnd_client.initialize_wallet.assert_called()
+        keyring_patch.set_password.assert_called()
