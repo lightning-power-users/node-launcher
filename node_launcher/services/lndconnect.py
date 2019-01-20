e730383d3265fa2b062a5e193e0c4d769d343e46
Manuel Costa <manuel.costa@kelvininc.com>
2019-01-20 18:41:27 +0000

    converting from base64 to base64URL using the oportunity to remove a duplicate requirement

--- node_launcher/services/lndconnect.py
+++ node_launcher/services/lndconnect.py
@@ -2,6 +2,10 @@ import base64
 import qrcode
 
 
+def base64URL_from_base64(s):
+    return s.replace('+', '-').replace('/', '_').rstrip('=')
+
+
 def get_deprecated_lndconnect_url(host, port, cert_path, macaroon_path):
     return f'lndconnect:?cert={cert_path}&macaroon={macaroon_path}&host={host}:{port}'
 
@@ -11,9 +15,11 @@ def get_lndconnect_url(host, port, cert_path, macaroon_path):
         lines = cert_file.read().split('\n')
         lines = [line for line in lines if line != '']
         cert = ''.join(lines[1:-1])
+        cert = base64URL_from_base64(cert)
 
     with open(macaroon_path, 'rb') as macaroon_file:
         macaroon = base64.b64encode(macaroon_file.read()).decode('ascii')
+        macaroon = base64URL_from_base64(macaroon)
 
     return f'lndconnect://{host}:{port}?cert={cert}&macaroon={macaroon}'
 
