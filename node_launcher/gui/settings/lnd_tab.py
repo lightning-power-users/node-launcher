7b465c5087ab79d022faac48bc3bf097241ea2b9
Pierre Rochard <pierre@rochard.org>
2019-02-10 20:06:33 -0500

    refactor lnd conf tab data update

--- node_launcher/gui/settings/lnd_tab.py
+++ node_launcher/gui/settings/lnd_tab.py
@@ -17,19 +17,17 @@ class LndTab(QWidget):
         color = self.lnd.file['color']
         self.alias_layout.set_color(color)
         self.alias_layout.new_color.connect(
-            self.set_new_color
+            lambda x: self.set_conf_value('color', x)
         )
 
         self.alias_layout.alias_editor.textEdited.connect(
-            self.set_new_alias
+            lambda x: self.set_conf_value('alias', x)
         )
         self.lnd_layout = QGridLayout()
         self.lnd_layout.addLayout(self.alias_layout, column_span=2)
 
         self.setLayout(self.lnd_layout)
 
-    def set_new_alias(self, new_alias: str):
-        self.lnd.file['alias'] = new_alias
+    def set_conf_value(self, key: str, new_value: str):
+        self.lnd.file[key] = new_value
 
-    def set_new_color(self, new_color: str):
-        self.lnd.file['color'] = new_color
