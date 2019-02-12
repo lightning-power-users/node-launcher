6e66ee69af9d8c71d8caadcd61d82aeeab69e939
Hannes Kruger <hkruger0718@gmail.com>
2019-02-13 00:10:08 +0200

    Added RestartLayout to alert user when restart is needed to Lnd or Bitcoin node due to config file changes

--- node_launcher/gui/network_buttons/__init__.py
+++ node_launcher/gui/network_buttons/__init__.py
@@ -2,3 +2,4 @@ from .joule_layout import JouleLayout
 from .lnd_wallet_layout import LndWalletLayout
 from .nodes_layout import NodesLayout
 from node_launcher.gui.network_buttons.advanced.zap_layout import ZapLayout
+from .restart_layout import RestartLayout
