6b494074b3d4e85f8619337a4ca6b93d516a16d6
Pierre Rochard <pierre@rochard.org>
2019-02-13 14:00:16 -0500

    iterate on request capacity

--- website/views/request_capacity_view.py
+++ website/views/request_capacity_view.py
@@ -1,6 +1,8 @@
+from bitcoin.core import COIN
 from flask import request, render_template
 from flask_admin import BaseView, expose
 
+from node_launcher.logging import log
 from node_launcher.node_set import NodeSet
 from website.forms.request_capacity_form import RequestCapacityForm
 from website.utilities.cache.cache import get_latest
@@ -13,16 +15,21 @@ class RequestCapacityView(BaseView):
         form = RequestCapacityForm()
         price = get_latest('usd_price')
         last_price = price['last']
+        price_per_sat = last_price/COIN
         fee_estimates = get_latest('fee_estimate')
 
         fee_estimate_choices = []
+        previous_estimate = 0
         for fee_estimate in fee_estimates:
             estimated_fee_per_byte = fee_estimate['conservative']['feerate']
+            if estimated_fee_per_byte == previous_estimate:
+                continue
+            previous_estimate = estimated_fee_per_byte
             select_label_time_estimate = fee_estimate['label'].replace('_', ' ').capitalize()
             if estimated_fee_per_byte > 1:
-                select_label = f'{select_label_time_estimate} {estimated_fee_per_byte} sats per byte'
+                select_label = f'{select_label_time_estimate} ({estimated_fee_per_byte} sats per byte)'
             else:
-                select_label = f'{select_label_time_estimate} 1 sat per byte'
+                select_label = f'{select_label_time_estimate} (1 sat per byte)'
             select_value = estimated_fee_per_byte
             fee_estimate_choices.append((select_value, select_label))
         form.fee_rate.choices = fee_estimate_choices
@@ -42,20 +49,18 @@ class RequestCapacityView(BaseView):
         return render_template('request_capacity.html',
                                form=form,
                                address=address,
-                               last_price=last_price)
+                               price_per_sat=price_per_sat)
 
-    @expose('/buy_capacity', methods=['GET', 'POST'])
-    def buy_channel(self):
+    @expose('/process_request', methods=['GET', 'POST'])
+    def process_request(self):
         if request.method == 'POST':
-            value = int(request.form['value'])
-            memo = request.form['memo']
-        else:
-            value = 50000
-            memo = 'Tip'
+            data = request.form
+            log.info('request-capacity.process_request POST', data=data)
+
         node_set = NodeSet()
         payment_request = node_set.lnd_client.create_invoice(
-            value=value,
-            memo=memo
+            value=100,
+            memo='Capacity request'
         ).payment_request
         uri = ':'.join(['lightning', payment_request])
 
