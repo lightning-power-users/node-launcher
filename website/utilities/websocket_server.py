e35a02a62df7c50a6ac53025e08beaf7942833ed
Pierre Rochard <pierre@rochard.org>
2019-02-15 15:01:57 -0500

    refactor websocket server

--- website/utilities/websocket_server.py
+++ website/utilities/websocket_server.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import time
 
 import websockets
 from google.protobuf.json_format import MessageToDict
@@ -11,41 +12,42 @@ from website.constants import FLASK_SECRET_KEY
 
 async def serve_invoices(websocket, path):
     invoices = {}
-    subscription = lnd_remote_client.subscribe_invoices(settle_index=1)
-    while True:
-        tracker_data = await websocket.recv()
-        if tracker_data:
-            try:
-                tracker_data = json.loads(tracker_data)
-            except:
-                log.error('error loading json', exc_info=True)
-                continue
+    # while True:
+    tracker_data = await websocket.recv()
+    if tracker_data:
+        try:
+            tracker_data = json.loads(tracker_data)
+        except:
+            log.error('error loading json', exc_info=True)
+            return
 
-        tracker = tracker_data.get('tracker', None)
-        if not tracker:
-            continue
+    tracker = tracker_data.get('tracker', None)
+    if not tracker:
+        return
 
-        if tracker == FLASK_SECRET_KEY:
-            data = tracker_data
-            invoice_data = data['invoice']
-            invoices[invoice_data['r_hash']] = data
-            log.debug('received from server', data=data)
-            break
+    if tracker == FLASK_SECRET_KEY:
+        data = tracker_data
+        invoice_data = data['invoice']
+        invoices[invoice_data['r_hash']] = data
+        log.debug('received from server', data=data)
+        return
 
-        for invoice in subscription:
-            invoice_data = MessageToDict(invoice)
-            if invoice_data['r_hash'] not in invoices:
-                time.sleep(2)
-                await websocket.send(json.dumps(invoice_data))
-                break
-                # TODO: check on disk
-                log.warn('r_hash not found in memory', invoice_data=invoice_data)
-                continue
-            data = invoice_data['r_hash']
-            if data['tracker'] != tracker:
-                continue
-            log.debug('emit invoice', invoice=invoice)
+    invoice_subscription = lnd_remote_client.subscribe_invoices(settle_index=1)
+    for invoice in invoice_subscription:
+        invoice_data = MessageToDict(invoice)
+        if invoice_data['r_hash'] not in invoices:
+            time.sleep(2)
             await websocket.send(json.dumps(invoice_data))
+            break
+            # TODO: check on disk
+            log.warn('r_hash not found in memory', invoice_data=invoice_data)
+            continue
+        data = invoice_data['r_hash']
+        if data['tracker'] != tracker:
+            continue
+        log.debug('emit invoice', invoice=invoice)
+        await websocket.send(json.dumps(invoice_data))
+        break
 
 
 start_server = websockets.serve(serve_invoices, 'localhost', 8765)
