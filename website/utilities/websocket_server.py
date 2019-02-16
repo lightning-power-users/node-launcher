301946bb2a51b8dc07ec2e6ae9e361f1fb1e24c8
Pierre Rochard <pierre@rochard.org>
2019-02-15 21:43:42 -0500

    fill out section for channel opening

--- website/utilities/websocket_server.py
+++ website/utilities/websocket_server.py
@@ -4,15 +4,17 @@ import time
 
 import websockets
 from google.protobuf.json_format import MessageToDict
+from grpc._channel import _Rendezvous
 
 from node_launcher.logging import log
 from tools.lnd_client import lnd_remote_client
 from website.constants import FLASK_SECRET_KEY
 
 
+channel_opening_invoices = {}
+
+
 async def serve_invoices(websocket, path):
-    invoices = {}
-    # while True:
     tracker_data = await websocket.recv()
     if tracker_data:
         try:
@@ -28,30 +30,54 @@ async def serve_invoices(websocket, path):
     if tracker == FLASK_SECRET_KEY:
         data = tracker_data
         invoice_data = data['invoice']
-        invoices[invoice_data['r_hash']] = data
+        # Todo: populate invoices from disk as well
+        channel_opening_invoices[invoice_data['r_hash']] = data
         log.debug('received from server', data=data)
         return
 
+    channel_opening_data = None
     invoice_subscription = lnd_remote_client.subscribe_invoices(settle_index=1)
     for invoice in invoice_subscription:
         invoice_data = MessageToDict(invoice)
-        if invoice_data['r_hash'] not in invoices:
+        # Todo: always resolves to true for testing, remove
+        if not True and invoice_data['r_hash'] not in channel_opening_invoices:
+            # TODO: remove
             time.sleep(2)
-            await websocket.send(json.dumps(invoice_data))
-            break
+
             # TODO: check on disk
             log.warn('r_hash not found in memory', invoice_data=invoice_data)
             continue
-        data = invoice_data['r_hash']
-        if data['tracker'] != tracker:
+        channel_opening_data = channel_opening_invoices[invoice_data['r_hash']]
+        if channel_opening_data.get('tracker', None) != tracker:
             continue
         log.debug('emit invoice', invoice=invoice)
         await websocket.send(json.dumps(invoice_data))
         break
 
+    if channel_opening_data is not None:
+        if channel_opening_data.get('reciprocation_capacity', None):
+            local_funding_amount = channel_opening_data['reciprocation_capacity']
+        else:
+            local_funding_amount = int(channel_opening_data['form_data']['capacity'])
 
+        sat_per_byte = int(channel_opening_data['form_data']['transaction_fee_rate'])
 
+        response = lnd_remote_client.open_channel(
+            node_pubkey_string=channel_opening_data['parsed_pubkey'],
+            local_funding_amount=local_funding_amount,
+            push_sat=0,
+            sat_per_byte=sat_per_byte,
+            spend_unconfirmed=True
+        )
 
+        try:
+            for update in response:
+                update_data = {'open_channel_update': MessageToDict(update)}
+                await websocket.send(json.dumps(update_data))
+        except _Rendezvous as e:
+            error_details = e.details()
+            error_message = {'error': error_details}
+            await websocket.send(json.dumps(error_message))
 
 start_server = websockets.serve(serve_invoices, 'localhost', 8765)
 
