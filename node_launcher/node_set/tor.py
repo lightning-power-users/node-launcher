692568d1ef580538969474bcccb6aea3181e3229
vindard <pgp@arvinda.me>
2019-03-27 01:33:17 -0400

    set next TODOs

--- node_launcher/node_set/tor.py
+++ node_launcher/node_set/tor.py
@@ -45,8 +45,8 @@ class Tor(object):
         self.lnd.file['tor.streamisolation'] = '1'
 
 
-        # TODO figure out how to actually replace existing Tor conf modifications with this Class
-        # TODO figure how to roll existing `launch` functions into this Class
+        # TODO next, figure how to roll existing `launch` functions into this Class (Ctrl+Shift+F run_tor)
+        # TODO next, figure out how to actually replace existing Tor conf modifications with this Class
 
     def launch(self):
         pass
