4e6ccad449b48c8137116d3e6ae26c9f70c208f7
vindard <pgp@arvinda.me>
2019-03-29 16:39:52 -0400

    update 'config file custom assignment operator' test

--- tests/test_services/test_configuration_file.py
+++ tests/test_services/test_configuration_file.py
@@ -49,11 +49,12 @@ class TestConfigurationFile(object):
 
     def test_assign_op(self, configuration_file: ConfigurationFile):
         configuration_file['key'] = 'value'
-        with open(configuration_file.path, 'r') as f:
+        new_object = ConfigurationFile(configuration_file.path, ' ')
+        with open(new_object.path, 'r') as f:
             text = f.read()
             assert 'key=value' in text
-        new_object = ConfigurationFile(configuration_file.path, ' ')
         new_object['key'] = 'value'
         with open(new_object.path, 'r') as f:
             text = f.read()
+            assert 'key=value' not in text
             assert 'key value' in text
