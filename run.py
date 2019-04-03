5b87deb046c6aa3b7c02b3b583f73c4b70fe73ad
Pierre Rochard <pierre@rochard.org>
2019-04-03 09:07:59 -0400

    re-enable the except_hook that provides debugging information

--- run.py
+++ run.py
@@ -6,9 +6,10 @@ from node_launcher.constants import (
 )
 from node_launcher.gui.application import Application
 from node_launcher.logging import log
+from node_launcher.utilities.except_hook import except_hook
 
 if __name__ == '__main__':
-    # sys.excepthook = except_hook
+    sys.excepthook = except_hook
 
     log.info(
         'constants',
