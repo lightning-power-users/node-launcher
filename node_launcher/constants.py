ce802e0ebb92f423a0cdea8f1425fbe16820d6ae
Pierre Rochard <pierre@rochard.org>
2019-04-05 21:30:41 -0400

    Bump LND version!

--- node_launcher/constants.py
+++ node_launcher/constants.py
@@ -3,10 +3,10 @@ from os.path import expanduser
 import platform
 from typing import Dict
 
-NODE_LAUNCHER_RELEASE = '.'.join(map(str, (6, 0, 1)))
+NODE_LAUNCHER_RELEASE = '.'.join(map(str, (6, 0, 2)))
 
 TARGET_BITCOIN_RELEASE = 'v0.17.1'
-TARGET_LND_RELEASE = 'v0.5.2-beta'
+TARGET_LND_RELEASE = 'v0.6-beta-rc3'
 
 
 class StringConstant(object):
