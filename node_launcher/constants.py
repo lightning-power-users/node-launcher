e3e3df98ec8daa8e0563c7d77567c2f6a9e33e95
willcl-ark <will8clark@gmail.com>
2019-04-12 09:29:12 +0100

    Updated LND version to v0.6-beta-rc4 as rc3 no longer hosted
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- node_launcher/constants.py
+++ node_launcher/constants.py
@@ -6,7 +6,7 @@ from typing import Dict
 NODE_LAUNCHER_RELEASE = '.'.join(map(str, (6, 0, 3)))
 
 TARGET_BITCOIN_RELEASE = 'v0.17.1'
-TARGET_LND_RELEASE = 'v0.6-beta-rc3'
+TARGET_LND_RELEASE = 'v0.6-beta-rc4'
 
 
 class StringConstant(object):
