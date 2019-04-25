a58cba65f5f758c5c94d248d8fbe46c4688728df
Pierre Rochard <pierre@rochard.org>
2019-04-24 19:43:17 -0700

    remove unnecessary tor process class

--- node_launcher/node_set/tor/tor_node.py
+++ node_launcher/node_set/tor/tor_node.py
@@ -1,18 +1,25 @@
+from node_launcher.node_set.lib.managed_process import ManagedProcess
 from node_launcher.node_set.lib.network_node import NetworkNode
+from node_launcher.node_set.lib.node_status import NodeStatus
 from .tor_configuration import TorConfiguration
-from .tor_process import TorProcess
 from .tor_software import TorSoftware
 
 
 class TorNode(NetworkNode):
     software: TorSoftware
     configuration: TorConfiguration
-    process: TorProcess
+    process: ManagedProcess
 
     def __init__(self):
         super().__init__(
             network='tor',
             Software=TorSoftware,
             Configuration=TorConfiguration,
-            Process=TorProcess
+            Process=ManagedProcess
         )
+
+    def handle_log_line(self, log_line: str):
+        if 'Bootstrapped 0%: Starting' in log_line:
+            self.update_status(NodeStatus.SYNCING)
+        elif 'Bootstrapped 100%: Done' in log_line:
+            self.update_status(NodeStatus.SYNCED)
