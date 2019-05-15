23ea1381130b89c437ffdf4e8c2630e189f7e684
Pierre Rochard <pierre@rochard.org>
2019-05-14 21:17:42 -0400

    Fix update key function in configuration widget

--- node_launcher/gui/menu/node_manager/configuration_widget.py
+++ node_launcher/gui/menu/node_manager/configuration_widget.py
@@ -23,7 +23,7 @@ class ConfigurationWidget(QTableWidget):
 
         self.cellChanged.connect(self.handle_cell_change)
 
-        self.setHorizontalHeaderLabels(['Node', 'Property', 'Value'])
+        self.setHorizontalHeaderLabels(['Node Name', 'Key', 'Value'])
 
     def get(self, node_name: str, key: str) -> List[Tuple[int, str]]:
         value_key_items = list(self.findItems(key, Qt.MatchExactly))
@@ -35,6 +35,9 @@ class ConfigurationWidget(QTableWidget):
     def append_key_value(self, node_name: str, key: str, new_value: str):
         row_number = self.rowCount()
         self.insertRow(row_number)
+        self.update_row(row_number, node_name, key, new_value)
+
+    def update_row(self, row_number, node_name, key, new_value):
         for column_index, cell_text in enumerate([node_name, key, new_value]):
             item = QTableWidgetItem()
             item.setText(cell_text)
@@ -46,25 +49,31 @@ class ConfigurationWidget(QTableWidget):
         self.resizeColumnsToContents()
 
     def update_key(self, node_name: str, key: str, new_values: List[str]):
-        existing_value_items = self.get(node_name, key)
-        for row_index, value in existing_value_items:
-            self.removeRow(row_index)
-        for new_value in new_values:
-            self.append_key_value(node_name, key, new_value)
+        existing_value_items: List[Tuple[int, str]] = self.get(node_name, key)
+        if len(existing_value_items) > len(new_values):
+            for row_index, _ in existing_value_items:
+                self.removeRow(row_index)
+
+        for new_value_index, new_value in enumerate(new_values):
+            if new_value_index < len(existing_value_items):
+                self.update_row(
+                    row_number=existing_value_items[new_value_index][0],
+                    node_name=node_name,
+                    key=key,
+                    new_value=new_value
+                )
+            elif new_value_index >= len(existing_value_items):
+                self.append_key_value(node_name, key, new_value)
 
     def handle_cell_change(self, row: int, column: int):
-        if column != 2:
-            return
+        if column == 2:
+            self.handle_value_cell_change(row=row)
 
-        node_item = self.item(row, 0)
-        key_item = self.item(row, 1)
-        if node_item is None or key_item is None:
-            return
-        node_name = node_item.text()
-        key = key_item.text()
+    def handle_value_cell_change(self, row: int):
+        node_name = self.item(row, 0).text()
+        key = self.item(row, 1).text()
         new_values = [v[1] for v in self.get(node_name, key)]
 
-        node_name = self.item(row, 0).text()
         if node_name == 'bitcoind':
             config = self.node_set.bitcoind_node.configuration
         elif node_name == 'lnd':
