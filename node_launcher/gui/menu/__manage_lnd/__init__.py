e212e5909bcfd10f508eb5d84d749400c3989add
Manuel Costa <manuel.costa@kelvininc.com>
2019-05-21 21:16:33 +0100

    removing old files

--- node_launcher/gui/menu/__manage_lnd/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lnd_configuration_tab import LndConfigurationTab
-from .lnd_restart_layout import LndRestartLayout
-from .tls_layout import TlsLayout
-from .zap_qrcode_label import ZapQrcodeLabel
