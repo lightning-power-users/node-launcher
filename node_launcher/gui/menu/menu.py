4d08481a7dfc9d437c28f947d2bfe35107e17d7c
Pierre Rochard <pierre@rochard.org>
2022-01-11 14:26:02 -0600

    Simplify GUI menu

--- node_launcher/gui/menu/menu.py
+++ node_launcher/gui/menu/menu.py
@@ -1,11 +1,5 @@
-from node_launcher.gui.qt import QCoreApplication, QGuiApplication, QMenu, QAction
-from node_launcher.gui.menu.menu_actions.nodes_menu.bitcoind_status_action import \
-    BitcoindStatusAction
-from node_launcher.gui.menu.menu_actions.nodes_menu.lnd_status_action import \
-    LndStatusAction
-from node_launcher.gui.menu.menu_actions.separator_action import SeparatorAction
-from node_launcher.gui.menu.menu_actions.nodes_menu.tor_status_action import \
-    TorStatusAction
+from node_launcher.gui.menu.menu_actions.menu_action import MenuAction
+from node_launcher.gui.qt import QCoreApplication, QGuiApplication, QMenu
 from node_launcher.gui.reveal_directory import reveal_directory
 from node_launcher.node_set import NodeSet
 
@@ -15,25 +9,12 @@ class Menu(QMenu):
         super().__init__()
         self.node_set = node_set
         self.system_tray = system_tray
-        self.cache = []
 
-        default_actions = [
-            TorStatusAction(self.node_set.tor_node, parent=parent),
-            BitcoindStatusAction(self.node_set.bitcoind_node, parent=parent),
-            LndStatusAction(self.node_set.lnd_node, parent=parent),
-            SeparatorAction()
-        ]
-
-        for action in default_actions:
-            self.add_action(action)
-
-        # Joule
-        self.joule_status_action = self.addAction('Joule Browser UI')
-        self.joule_status_action.setEnabled(False)
-        self.joule_url_action = self.addAction('Copy Node URL (REST)')
-        self.joule_macaroons_action = self.addAction('Show Macaroons')
-        self.joule_url_action.triggered.connect(self.copy_rest_url)
-        self.joule_macaroons_action.triggered.connect(self.reveal_macaroon_path)
+        self.node_set_status_action = MenuAction('Connecting...')
+        self.node_set_status_action.setText('Connecting...')
+        self.node_set_status_action.setEnabled(False)
+        self.setVisible(True)
+        self.addAction(self.node_set_status_action)
 
         self.addSeparator()
         # Todo: add debug
@@ -43,10 +24,6 @@ class Menu(QMenu):
         self.quit_action.triggered.connect(
             lambda _: QCoreApplication.exit(0)
         )
-
-    def add_action(self, action: QAction):
-        self.cache.append(action)
-        self.addAction(action)
 
     def copy_rest_url(self):
         QGuiApplication.clipboard().setText(self.node_set.lnd_node.configuration.rest_url)
